2ed0d942fd60cbd90888cf5ec9f065e2ab03d152
“Il_Roman” <il.roman@ukr.net>
2020-02-07 19:19:44 +0200

    commit

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Greeting = props => {
     return (
         <div>
-            {`My name is ${props.firstName} ${props.lastName}. I'm ${props.age} years old`}
+            <h1>
+                {`My name is ${props.firstName} ${props.lastName}. I'm ${props.age} years old`}
+            </h1>
         </div>
     )
 }
