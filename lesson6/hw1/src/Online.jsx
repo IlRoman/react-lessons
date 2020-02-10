0527f9d032e060219779829e53b3155916992f5d
“Il_Roman” <il.roman@ukr.net>
2020-02-10 14:08:15 +0200

    commit

---
+++ lesson6/hw1/src/Online.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Online = () => {
+    return <span className="status__text">Online</span>
+};
+
+export default Online;
---
+++ lesson6/hw1/src/Status.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Online from './Online';
+import Offline from './Offline';
+
+const Status = isOnline => {
+    return (
+        <div className="status" >
+            {isOnline == true
+                ? <Online />
+                : <Offline />
+            }
+        </div>
+    )
+}
+
+export default Status;
