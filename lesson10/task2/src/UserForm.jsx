f099fa5478fa3bf0697018bfb4e449a04e7b9c2f
“Il_Roman” <il.roman@ukr.net>
2020-02-18 13:16:28 +0200

    commit

---
+++ lesson10/task2/src/UserForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const UserForm = ({ userData, handleChange }) => {
+    const onChange = event => {
+        handleChange(event);
+    }
+
+    return (
+        <form className="user-form">
+            <input
+                type="text"
+                name="firstName"
+                className="user-form__input"
+                value={userData.firstName}
+                onChange={onChange}
+            />
+            <input
+                type="text"
+                name="lastName"
+                className="user-form__input"
+                value={userData.lastName}
+                onChange={onChange}
+            />
+        </form>
+    );
+};
+
+export default UserForm;
