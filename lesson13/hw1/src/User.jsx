6874b35f718814cedb76d76f088fe48b301463a7
“Il_Roman” <il.roman@ukr.net>
2020-02-29 10:58:35 +0200

    commit

---
+++ lesson13/hw1/src/User.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class User extends React.Component {
+    state = {
+        userName: '',
+        userAvatar: '',
+        userLocation: '',
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.match.url !== this.props.match.url) {
+            this.fetchUserData(this.props.match.url);
+        }
+    }
+
+    componentDidMount() {
+        this.fetchUserData(this.props.match.url);
+    }
+
+    fetchUserData = (userPath) => {
+        fetch(`https://api.github.com${userPath}`)
+            .then(response => response.json())
+            .then(user => this.setState({
+                userName: user.name,
+                userAvatar: user.avatar_url,
+                userLocation: user.location
+            }));
+    }
+
+    render() {
+        const { userName, userLocation, userAvatar } = this.state;
+        return (
+            <div className="user">
+                <img alt="User Avatar" src={userAvatar} className="user__avatar" />
+                <div className="user__info">
+                    <span className="user__name">{userName}</span>
+                    <span className="user__location">{userLocation}</span>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default User;
