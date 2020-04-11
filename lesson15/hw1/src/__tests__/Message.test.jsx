35c659b135b9d977f65b587edd16d63c038c8fa8
“Il_Roman” <il.roman@ukr.net>
2020-04-11 15:57:27 +0300

    commit

---
+++ lesson15/hw1/src/__tests__/Message.test.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Message from '../Message';
+import { shallow } from 'enzyme';
+
+describe('<Message />', () => {
+    it('should render text', () => {
+        const props = { text: 'Hello world' }
+        const wrappedComponent = shallow(<Message {...props} />);
+        expect(wrappedComponent).toMatchSnapshot('Hello world');
+    }),
+
+        it('should not render text', () => {
+            const props = { text: '' }
+            const wrappedComponent = shallow(<Message {...props} />);
+            expect(wrappedComponent).toMatchSnapshot();
+        })
+
+})
---
+++ lesson15/hw1/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Message from './Message';
+
+ReactDOM.render(<Message />, document.querySelector('#root'));
